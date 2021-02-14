f9e67b3c821cc3422b845f8dfcaa08524cabca65
Naoki Yahata <funkinaoki@gmail.com>
2021-02-14 23:18:22 +0900

    handin

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    //floatは小数のための変数の方
+    var count: Float = 0.00
+    
+    var timer: Timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func start(){
+        
+        if !timer.isValid{
+            
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true)
+        }
+    }
+    
+    @IBAction func stop(){
+        if timer.isValid{
+            timer.invalidate()
+        }
+    }
+    
+    @objc func up(){
+        count = count + 0.01
+        
+        label.text = String(format: "%.2f", count)
     }
 
 
